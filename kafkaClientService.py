9725b8bec8092cfecc44417a07516f746784c1f5
sunil karki <sunilkarki320@gmail.com>
2023-05-24 13:59:25 +0545

    Added changes to transfer mail and store its detail

--- kafkaClientService.py
+++ kafkaClientService.py
@@ -4,22 +4,49 @@ from json import loads
 
 # Kafka configuration
 bootstrap_servers = 'localhost:9092'
-email_requests_topic = 'email_requests'
-email_responses_topic = 'email_responses'
+email_requests_topic1 = 'email_requests'
+email_responses_topic1 = 'email_responses'
+
+email_requests_topic2 = 'email_requestsecond'
+email_responses_topic2 = 'email_responsesecond'
 
 
-def send_email_request(producer, email_from, email_to, message):
+def send_email_request1(producer, email_from, email_to, message, subject, template_flag):
+    email_request = {
+        'emailFrom': email_from,
+        'emailTo': email_to,
+        'message': message,
+        'subject': subject,
+        'templateFlag': template_flag
+    }
+    producer.send(email_requests_topic1, value=email_request)
+    print("producer.send1")
+    producer.flush()
+
+
+def send_email_request2(producer, email_from, email_to, message):
     email_request = {
         'emailFrom': email_from,
         'emailTo': email_to,
         'message': message
     }
-    producer.send(email_requests_topic, value=email_request)
-    print("producer.send")
+    producer.send(email_requests_topic2, value=email_request)
+    print("producer.send2")
     producer.flush()
 
 
-def get_email_details_response(consumer):
+def get_email_details_response1(consumer):
+    print("-" + str(consumer))
+    # print("-" + str(consumer["acknowledgement"]))
+    for message in consumer:
+        print("---")
+        email_response = message.value.decode('utf-8')
+        # email_response = message.value.decode('utf-8')
+        # email_response = eval(email_response)
+        print(email_response)
+
+
+def get_email_details_response2(consumer):
     for message in consumer:
         email_response = message.value
         # email_response = message.value.decode('utf-8')
@@ -38,30 +65,42 @@ def get_email_details_response(consumer):
 def run():
     print("Client")
     # producer = KafkaProducer(bootstrap_servers=bootstrap_servers)
-    producer = KafkaProducer(bootstrap_servers=['localhost:9092'],
-                             value_serializer=lambda x:
-                             dumps(x).encode('utf-8'))
-    consumer = KafkaConsumer(email_responses_topic, bootstrap_servers=['localhost:9092'],
-                             # auto_offset_reset='earliest',
-                             enable_auto_commit=True,
-                             # group_id='my-group',
-                             value_deserializer=lambda x: loads(x.decode('utf-8'))
-                             )
+    producer1 = KafkaProducer(bootstrap_servers=['localhost:9092'],
+                              value_serializer=lambda x:
+                              dumps(x).encode('utf-8'))
+    consumer1 = KafkaConsumer(email_responses_topic1, bootstrap_servers=['localhost:9092'],
+                              auto_offset_reset='earliest',
+                              enable_auto_commit=True,
+                              # group_id='my-group',
+                              value_deserializer=lambda x: loads(x.decode('utf-8'))
+                              )
 
-    email_from = "sender@example.com"
-    email_to = "recipient@example.com"
+    producer2 = KafkaProducer(bootstrap_servers=['localhost:9092'],
+                              value_serializer=lambda x:
+                              dumps(x).encode('utf-8'))
+    consumer2 = KafkaConsumer(email_responses_topic2, bootstrap_servers=['localhost:9092'],
+                              auto_offset_reset='earliest',
+                              enable_auto_commit=True,
+                              # group_id='my-group',
+                              value_deserializer=lambda x: loads(x.decode('utf-8'))
+                              )
+
+    email_from = "sunilkarki320@gmail.com"
+    email_to = "sunilkarki520@gmail.com"
     message = "Hello, this is a test email."
-    send_email_request(producer, email_from, email_to, message)
-    print("Email sent.")
+    subject = "test email."
+    template_flag = "1"
+    send_email_request1(producer1, email_from, email_to, message, subject, template_flag)
+    print("Email sent to first topic")
+    # get_email_details_response1(consumer1)
+
+    print("---------------")
 
     # Request 2: Get email details
     email_address = "test2@gmail.com"
-    send_email_request(producer, "", email_address, "")
-    email_from, email_to, message = get_email_details_response(consumer)
-    print("Email details for", email_address)
-    print("From:", email_from)
-    print("To:", email_to)
-    print("Message:", message)
+    send_email_request2(producer2, "", email_address, "")
+    get_email_details_response2(consumer2)
+    print("Response fetched from second topic")
 
 
 if __name__ == '__main__':
--- kafkaServerService.py
+++ kafkaServerService.py
@@ -1,10 +1,109 @@
 from kafka import KafkaConsumer, KafkaProducer
+from pymongo import MongoClient
 from json import dumps
+
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+
+# import time
+# import email.message
+import smtplib
 
 # Kafka configuration
 bootstrap_servers = 'localhost:9092'
 email_requests_topic = 'email_requests'
 email_responses_topic = 'email_responses'
+
+# MongoDB configuration
+mongodb_host = 'localhost'
+mongodb_port = 27017
+mongodb_database = 'email_db'
+mongodb_collection = 'emails'
+mongodb_collection_template = 'emailtemplate'
+
+
+def loadEmailTemplate(template_flag, email_message):
+    client = MongoClient("mongodb://" + mongodb_host + ":" + str(mongodb_port))
+    db = client[mongodb_database]
+    collection = db[mongodb_collection_template]
+
+    # Query email details from MongoDB
+    query = {"template_flag": template_flag}
+
+    try:
+        template_record = collection.find_one(query)
+        if template_record:
+            template_msg = template_record['template']
+
+            template_msg.replace("<msg>", email_message)
+            print('template_msg: ' + template_msg)
+
+            return template_msg, "SUCCESS"
+
+        else:
+            print("Could not find the template specified!!!")
+            return email_message, "ERROR"
+
+    except Exception as e:
+        raise Exception("Error found with Database!! " + str(e))
+
+
+def insertToDB(email_to, email_from, subject, message, email_status):
+    print("Inserting record to the Database ...")
+
+    client = MongoClient("mongodb://" + mongodb_host + ":" + str(mongodb_port))
+    db = client[mongodb_database]
+    collection = db[mongodb_collection]
+
+    record = {
+        'subject': subject,
+        'emailfrom': email_from,
+        'emailto': email_to,
+        'emailbody': message,
+        'emailstatus': email_status
+    }
+
+    try:
+        # Insert the record for Storage
+        email_result = collection.insert_one(record)
+        print(email_result)
+        return "SUCCESS"
+    except Exception as e:
+        print("Insertion failed to the Database!!")
+        status_msg = str(e)
+        print(status_msg)
+        return status_msg
+
+
+def sendEmail(email_to, email_from, subject, email_message):
+    print("Sending mail")
+    # time.sleep(20)  # delay for 5 seconds
+
+    try:
+        sender_address = "testmailt828@gmail.com"
+        sender_pass = 'Twilio123'
+
+        message = MIMEMultipart()
+        message['From'] = sender_address
+        message['To'] = email_to
+        message['Subject'] = subject
+        message.attach(MIMEText(email_message, 'plain'))
+
+        session = smtplib.SMTP('smtp.gmail.com', 587)
+        session.starttls()
+        session.login(sender_address, sender_pass)
+        text = message.as_string()
+
+        session.sendmail(sender_address, email_to, text)
+        session.quit()
+
+        print("Email was sent to: " + email_to)
+        return "SUCCESS"
+    except Exception as e:
+        print("Email NOT sent to: " + email_to)
+        status_msg = str(e)
+        print(status_msg)
+        return status_msg
 
 
 def process_email_requests(consumer, producer):
@@ -15,14 +114,32 @@ def process_email_requests(consumer, producer):
         print(email_request)
         email_from = email_request['emailFrom']
         email_to = email_request['emailTo']
-        message = email_request['message']
+        email_message = email_request['message']
+        email_subject = email_request['subject']
+        template_flag = email_request['templateFlag']
+        # template_flag = 1
 
-        # Process email request
+        print(template_flag)
 
-        # Send acknowledgement
-        email_response = {'acknowledgement': 'Email received.'}
-        producer.send(email_responses_topic, value=email_response)
-        producer.flush()
+        try:
+            # Process email request
+            email_template, email_template_status = loadEmailTemplate(template_flag, email_message)
+            email_status = sendEmail(email_to, email_from, email_subject, email_template)
+            db_status = insertToDB(email_to, email_from, email_subject, email_message, email_status)
+
+            # Send acknowledgement
+            email_response = {
+                'acknowledgement': 'Email received.',
+                'emailStatus': email_status,
+                'dbStatus': db_status,
+                'emailTemplateStatus': email_template_status
+            }
+
+            producer.send(email_responses_topic, value=email_response)
+            producer.flush()
+            print("response sent...")
+        except Exception as e:
+            print(str(e))
 
 
 def run():
